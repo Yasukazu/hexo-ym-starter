3e921bc5b24a235fe197cdfd13bfb75a0a95c7fd
YasukazuMakino <mkn@yskz.dev>
2023-02-17 16:02:27 +0900

    space compress

--- source/js/search.js
+++ source/js/search.js
@@ -38,7 +38,7 @@ function analyzeData(document, query_str) {
   const query = normalized_query.replace(combining_chars_regex, '');
   const query_regex = RegExp(query);
   const test_children = [0, 2];
-  const test_items = ['title', 'content', 'tag', 'category']; // true: single, true: multiple
+  const test_items = ['title', 'content'];
   for (let entry of entries) {
     let match = false;
     let content = '';
@@ -67,41 +67,43 @@ function analyzeData(document, query_str) {
 
 /**
  * 
- * @param {Object< Array<Element>, Array<string> >} entries_items 
+ * @param {Array<Element>} entries
+ * @param {Array<string>} items 
  * @returns {Element}
  */
-function makeSearchResultFromTemplates (entries_items) {
-  const entries = entries_items.entries;
-  const items = entries_items.items;
+function makeSearchResultFromTemplates (entries, items) {
   let template_str = "template#search-result-container";
   let template = document.querySelector(template_str);
   if (!template) {
-    throw `${template_str} is not found!`;
+    throw `'${template_str}' is not found!`;
   }
   const search_result_container = document.importNode(template.content, true);
   const search_result_entries = search_result_container.querySelector('.entries');
   if (!search_result_entries) {
-    throw `.entries is not found!`;
+    throw `.entries is not found in search result container!`;
   }
 
   template_str = "template#search-result-entry";
   template = document.querySelector(template_str);
   if (!template) {
-    throw `${template_str} is not found!`;
+    throw `'${template_str}' template is not found!`;
   }
-  for (const [i, entry] of entries) {
+  for (const [i, entry] of entries.entries()) {
     const entry_output = document.importNode(template.content, true);
-    const title = entry.querySelector('title').textContent; // children[0]
+    const title = entry.querySelector('title')?.textContent; // children[0]
     if (!title) {
-      throw "No title!";
+      throw "No title in entry!";
     }
-    const url = entry.querySelector('url').textContent; // 2
+    const url = entry.querySelector('url')?.textContent; // 2
     if (!url) {
-      throw "No url!";
+      throw "No 'url' in entry!";
     }
 
     // const tags = entry.children[4].textContent
     const ar = entry_output.querySelector('a.title');
+    if (!ar) {
+      throw "No 'a' in template!"
+    }
     ar.href = url;
     ar.innerText = title;
     // pick up date from url beginning
@@ -115,18 +117,46 @@ function makeSearchResultFromTemplates (entries_items) {
 
     const ct = entry_output.querySelector('.content');
     if (ct){
-      const content = entry.children[3].textContent;
-      const content_tree = content ? new DOMParser().parseFromString(content, "text/html") : null;
-      const text_content = content_tree?.children[0]?.textContent;
-      if (text_content) {
-        // ct.innerText = content.replace(/<[^>]*>/g, '').substring(0, 300) + '...';
-        const chars = Array.from(text_content); // code point sequences
-        ct.innerText = chars.slice(0, 300).join();
+      const content = entry.querySelector('content')?.textContent;
+      if (content) {
+        const content_tree = new DOMParser().parseFromString(content, "text/html");
+        const text_content = content_tree?.children[0]?.textContent;
+        if (text_content) {
+          // ct.innerText = content.replace(/<[^>]*>/g, '').substring(0, 300) + '...';
+          const chars = Array.from(text_content); // code point sequences
+          const limitedStr = getFirstNChars(chars, 300);
+          // const innerText = chars.slice(0, 300).join('').replace(/\s+/g, ' ');
+          ct.innerText = limitedStr;
+        }
       }
     }
     search_result_entries.append(entry_output);
   }
   return search_result_container;
+}
+
+/**
+ * 
+ * @param {string[]} src 
+ * @param {Number} n 
+ * @returns {string}
+ */
+function getFirstNChars(src, n) {
+  let lc = '';
+  let i = 0;
+  let out = '';
+  for (let c of src) {
+    if (lc === ' ' && lc === c) {
+      continue;
+    }
+    else {
+      lc = c;
+    }
+    out += c;
+    if(++i >= n)
+      break;
+  }
+  return out;
 }
 
 /**
@@ -185,14 +215,14 @@ function search() {
   }
   // let search_result = `FetchData from ${fetch_path} with ${queryWord}`;
   fetch_data.then(document => {
-    const entries_items = analyzeData(document, queryWord); 
-    if (entries_items.length <= 0) {
+    const {entries, items} = analyzeData(document, queryWord); 
+    if (entries.length <= 0) {
       console.log("entries.length is zero.");
     }
     while (searchResult.firstChild) {
       searchResult.firstChild.remove();
     }
-    const search_result = makeSearchResultFromTemplates(entries_items)
+    const search_result = makeSearchResultFromTemplates(entries, items);
     if (search_result) {
       searchResult.append(search_result);
     }
